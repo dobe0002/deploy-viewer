49627935d57bb93a3bfbbc67579aec8309427a1c
Kim Doberstein <dobe0002@umn.edu>
2020-06-02 13:19:20 -0500

    Point to new spreadsheet

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,4 +1,5 @@
 export default {
-  googleId: '18077uj95WnuAV4cqucAbARLyRVjDPidziQC2eBDfxMk'
+  googleId: '1PRSxBl4Vz23WyigC8xO1mH2r96FGBbcM4MF8P33l_VU'
+  // googleId: '18077uj95WnuAV4cqucAbARLyRVjDPidziQC2eBDfxMk'
   // gitHubBaseName: '/dobe0002/BuildViewer/'
 };
